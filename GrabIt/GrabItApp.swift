8c8bdc26edfa39fad2dfcb3df3189819e1536d48
Suvidha Nakhawa <suvidha.nakhawa@gmail.com>
2022-02-04 20:19:31 -0500

    Initial Commit

---
+++ GrabIt/GrabItApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrabItApp.swift
+//  GrabIt
+//
+//  Created by Suvidha Nakhawa on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct GrabItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
